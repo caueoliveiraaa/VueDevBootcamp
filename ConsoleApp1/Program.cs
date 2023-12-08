4b6d07f5c96ae9944b5b3193d01acafa29fbd834
caueoliveiraaa <leo.siscobra@gmail.com>
2023-12-08 15:16:50 -0300

    Basics
    Learning the basics

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Current time: " + DateTime.Now);
+
+// Implicit using directives
+// using System;
+// using System.IO;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Net.Http;
+// using System.Threading;
+// using System.Threading.Tasks;
+
+namespace Basics
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Program started at" + DateTime.Now);
+        Console.WriteLine("Write your name: ");
+
+        var name = Console.ReadLine();
+        Console.WriteLine($"\nHello {name}");
+
+        Console.WriteLine($"\nPress any key to end this program: ");
+        Console.ReadKey(true);
+    }
+  }
+}
+
+
