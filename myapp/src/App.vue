ee9ce218cad18872a63c2a32177aed26398bc85a
caueoliveiraaa <leo.siscobra@gmail.com>
2023-12-24 17:56:37 -0300

    Learning about components

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,24 +1,30 @@
 <template>
+  <div>
+    {{greet}} {{name}}
+    <div v-text="channel"></div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      name: "Smith"
+  export default {
+    name: 'App',
+    data() {
+      return {
+        greet: "Hello",
+        name: "Bot",
+        channel: "Coding",
+      }
     }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style> 
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
