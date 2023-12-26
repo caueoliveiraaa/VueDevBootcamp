0fee6a054bfc3be8c0bc4ae609105cd8e5a06f5e
caueoliveiraaa <leo.siscobra@gmail.com>
2023-12-26 07:36:27 -0300

    v binding

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -4,6 +4,8 @@
     <div v-text="channel"></div>
     <div v-html="channel2"></div>
     <div v-html="hack"></div>
+    <h2 v-bind:id="idBind">Id binding</h2>
+    <button v-bind:disabled="disabledBind">Button binding</button>
   </div>
 </template>
 
@@ -17,6 +19,8 @@
         channel: "Coding",
         channel2: "<b>Coding<b>", 
         hack: `<a href="#" onclick="alert('You have been hacked!')">Click me</a>`,
+        idBind: "Binding",
+        disabledBind: true,
       }
     }
   }
