b543d5aac8b7a88428705a8024f6aa618d2d966a
caueoliveiraaa <leo.siscobra@gmail.com>
2024-01-21 14:22:38 -0300

    Methods
    Learning about methods

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-Vue.createApp({
+// View model
+const vm = Vue.createApp({
     data() {
         return {
             firstName: 'John',
             lastName: 'Smith'
         }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName}, ${this.lastName}`
+        }
     }
 }).mount('#app')
+
+setTimeout(() => {
+    vm.firstName = 'Bob'
+}, 2000)
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName:'Bryan',
+//             lastName:'Wade'
+//         }
+//     }
+// }).mount('#app2')
